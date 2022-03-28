89ed7c4c57a3ddfe535c119c56377edb1e5f2e3b
dg80 <dguillin80@gmail.com>
2022-03-28 22:46:18 +0200

    Add Events CurrentUserForArticles and hash password

---
+++ apiMS/src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\User;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface
+{
+    private UserPasswordHasherInterface $passwordHashed;
+
+    public function __construct(UserPasswordHasherInterface $passwordHashed)
+    {
+        $this->passwordHashed = $passwordHashed;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function encodePassword(ViewEvent $event): void
+    {
+        $user = $event->getControllerResult();
+        if ($user instanceof User) {
+            $passwordHashed = $this->passwordHashed->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($passwordHashed);
+        }
+    }
+}
