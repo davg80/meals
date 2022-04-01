dcd5848282b562f9bb44e12ce1aa639dd29d8f54
dg80 <dguillin80@gmail.com>
2022-04-01 20:04:52 +0200

    React router and routes

--- front-ms/src/App.js
+++ front-ms/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Article from './components/articles/Article'
+import Articles from './components/articles/Articles'
+import Account from './components/users/Account'
+import Profile from './components/users/Profile'
+import Login from './components/users/Login'
+import Register from './components/users/Register'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/articles" element={<Articles />} />
+      <Route exact path="/articles/:id" element={<Article />} />
+      <Route exact path="/account" element={<Account />} />
+      <Route exact path="/profile" element={<Profile />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/Login" element={<Login />} />
+    </Routes>
   );
 }
 
